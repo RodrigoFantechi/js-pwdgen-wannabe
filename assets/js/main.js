86ea2df405e6edd5a63f06a489bb29d2ba94ac4a
RodrigoFantechi <rodrifante595@libero.it>
2022-10-05 14:52:08 +0200

    add random password

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,20 @@ alert (password);
 document.getElementById('titlePassword').innerHTML = password;
 document.querySelector('h2').innerHTML = password;
 
+
+
+/* RANDOM */
+
+
+const nome1= prompt("inserire nome:");
+const cognome1= prompt("inserire cognome:");
+const colore1= prompt("inserire colore:");
+const password1 = nome1+cognome1+colore1+"22";
+let result = ' ';
+
+for(let i=0; i<password1.length; i++){
+    result+= password1.charAt(Math.random()*password1.length);
+}
+alert (result);
+document.getElementById('titlePassword2').innerHTML = result;
+
